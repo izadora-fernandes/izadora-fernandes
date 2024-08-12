56e411adc9481ff03319c1ceb402b2607884f16d
izadora-fernandes <izadora.cardoso.fernandes@escola.pr.gov.br>
2024-08-12 15:52:32 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(600, 600);
+  background("pink");
+}
+
+function draw() {
+
+  stroke("blue");
+  fill("purple");
+
+  //console.log (mouseIsPressed);
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
-  
-  function draw() {
-    background(220);
-  }
+}
